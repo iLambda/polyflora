1be6bb508c468b68f68e5092c6f570b2271834c8
iLambda <me@ilambda.io>
2024-10-18 20:38:21 +0200

    Added seed editor ; started moving state

--- src/app/blueprint/TreeBlueprintEditor.tsx
+++ src/app/blueprint/TreeBlueprintEditor.tsx
@@ -4,6 +4,7 @@ import { styles } from './TreeBlueprintEditor.css';
 import { Container, Tabs } from '@mantine/core';
 import { BranchEditor } from '@app/blueprint/editors/BranchEditor';
 import { useFloraStore } from '@app/state/Flora';
+import { SeedEditor } from '@app/blueprint/editors/SeedEditor';
 
 export const TreeBlueprintEditor = () => {
 
@@ -12,22 +13,25 @@ export const TreeBlueprintEditor = () => {
 
     /* Return component */
     return (
-        <Tabs defaultValue='trunk' className={styles.tabRoot}>
-            <Tabs.List>
-                <Tabs.Tab value='trunk'> Trunk </Tabs.Tab>
-                <Tabs.Tab value='branches'> Branches </Tabs.Tab>
-            </Tabs.List>
+        <>
+            <SeedEditor store={flora} />
+            <Tabs defaultValue='trunk' classNames={styles.tab} variant='outline'>
+                <Tabs.List>
+                    <Tabs.Tab value='trunk'> Trunk </Tabs.Tab>
+                    <Tabs.Tab value='branches'> Branches </Tabs.Tab>
+                </Tabs.List>
 
-            <Tabs.Panel value='trunk' className={styles.tabPanel}>
-                <Container className={styles.panelRoot}>
-                    <TrunkEditor store={flora.trunk} />
-                </Container>
-            </Tabs.Panel>
-            <Tabs.Panel value='branches' className={styles.tabPanel}>
-                <Container className={styles.panelRoot}>
-                    <BranchEditor store={flora.branch} />
-                </Container>
-            </Tabs.Panel>
-        </Tabs>
+                <Tabs.Panel value='trunk'>
+                    <Container className={styles.panelRoot}>
+                        <TrunkEditor store={flora.trunk} />
+                    </Container>
+                </Tabs.Panel>
+                <Tabs.Panel value='branches'>
+                    <Container className={styles.panelRoot}>
+                        <BranchEditor store={flora.branch} />
+                    </Container>
+                </Tabs.Panel>
+            </Tabs>
+        </>
     );
 };
