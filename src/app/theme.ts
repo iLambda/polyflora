d17a5a5fa3f022fa1d3ca948e00b047a28d195ac
Ada <me@ilambda.io>
2024-10-27 02:57:12 +0200

    Better green theming

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -3,4 +3,18 @@ import { createTheme } from '@mantine/core';
 // Do not forget to pass theme to MantineProvider
 export const theme = createTheme({
     primaryColor: 'green',
+    colors: {
+        green: [
+            '#ebfee7',
+            '#dbf8d4',
+            '#b7efab',
+            '#90e67e',
+            '#70de58',
+            '#5bd93f',
+            '#4fd731',
+            '#3fbe23',
+            '#34a91b',
+            '#25920e',
+        ],
+    },
 });
