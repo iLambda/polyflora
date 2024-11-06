6403a06984739073b4038496432d79c73e91eaa8
iLambda <me@ilambda.io>
2024-11-06 05:59:12 +0100

    Use clsx instead of hand merging

--- src/app/blueprint/editors/controls/Fieldgroup.tsx
+++ src/app/blueprint/editors/controls/Fieldgroup.tsx
@@ -1,20 +1,14 @@
 
 import { Fieldset, FieldsetProps } from '@mantine/core';
-import { useMemo } from 'react';
 import { styles } from './Fieldgroup.css';
+import clsx from 'clsx';
 
 export type FieldgroupProps = FieldsetProps;
 
 export const Fieldgroup = ({children, ...props}: FieldsetProps) => {
 
-    const classname = useMemo(() => 
-        [styles.fieldset, props.className]
-            .filter(str => str !== undefined)
-            .join(' ')
-        , [props.className]);
-
     return (
-        <Fieldset className={classname} {...props}>
+        <Fieldset className={clsx(styles.fieldset, props.className)} {...props}>
             { children }
         </Fieldset>
     );
